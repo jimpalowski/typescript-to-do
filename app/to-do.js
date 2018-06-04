53a048ea6aa530c0184e51a87b980a9064e3f468
Jim Palowski and Verna Santos <jim@>
2018-06-04 11:08:57 -0700

    Initial Commit

---
+++ app/to-do.js
@@ -0,0 +1,17 @@
+var Task = /** @class */ (function () {
+    function Task(description, priority) {
+        this.description = description;
+        this.priority = priority;
+        this.done = false;
+    }
+    return Task;
+}());
+var tasks = [];
+tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Buy chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+console.log(tasks);
+for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+    var task = tasks_1[_i];
+    console.log(task);
+}
